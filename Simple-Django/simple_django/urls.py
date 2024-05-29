b73a8e45266a487bdf6176424825b17c9ac2092d
Aakash Kumar Jha <aakash13102005@gmail.com>
2024-05-29 17:01:54 +0530

    initial commit

---
+++ Simple-Django/simple_django/urls.py
@@ -0,0 +1,12 @@
+ 
+from django.urls import path
+from django.views.generic import TemplateView
+from django.conf import settings
+ 
+urlpatterns = [
+     path(
+        '', 
+        TemplateView.as_view(template_name='home.html',    
+        extra_context={'magic': settings.MAGIC_MESSAGE})  # passing evironment value through url to template as magic 
+     ),
+]
